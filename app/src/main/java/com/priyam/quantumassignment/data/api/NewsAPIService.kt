6c3c01de4b79ada311543980f8967ceb6af897b2
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-06 05:25:01 +0530

    Retrofit

--- app/src/main/java/com/priyam/quantumassignment/data/api/NewsAPIService.kt
+++ app/src/main/java/com/priyam/quantumassignment/data/api/NewsAPIService.kt
@@ -1,5 +1,28 @@
 package com.priyam.quantumassignment.data.api
 
+import com.google.android.gms.common.api.Response
+import com.priyam.quantumassignment.data.model.APIResponse
+
 interface NewsAPIService {
-    
+    @GET("v2/top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country")
+        country:String,
+        @Query("page")
+        page:Int,
+        @Query("apiKey")
+        apiKey:String = BuildConfig.API_KEY
+    ): Response<APIResponse>
+
+    @GET("v2/top-headlines")
+    suspend fun getSearchedTopHeadlines(
+        @Query("country")
+        country:String,
+        @Query("q")
+        searchQuery:String,
+        @Query("page")
+        page:Int,
+        @Query("apiKey")
+        apiKey:String = BuildConfig.API_KEY
+    ): Response<APIResponse>
 }
