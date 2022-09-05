88f14d6094cc01436db50071aa818921fab2c50a
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-05 20:02:22 +0530

    Registraton layout completed

--- app/src/main/java/com/priyam/quantumassignment/MainActivity.kt
+++ app/src/main/java/com/priyam/quantumassignment/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.priyam.quantumassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var tabLayout: TabLayout
     lateinit var viewPager2: ViewPager2
+    lateinit var adapter: MyFragmentAdapter
 
 
 
@@ -17,5 +21,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tabLayout = findViewById(R.id.tabLayout)
         viewPager2 = findViewById(R.id.viewPager2)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Sign In"))
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"))
+
+        val fragmentManager : FragmentManager = supportFragmentManager
+        adapter = MyFragmentAdapter(fragmentManager, lifecycle)
+        viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager2.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+        viewPager2.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
     }
 }
