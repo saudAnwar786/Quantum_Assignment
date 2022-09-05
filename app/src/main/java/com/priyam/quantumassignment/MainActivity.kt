f2af77ea07503a7f6b6207dc9ea0ec12187172ed
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-05 19:18:43 +0530

    Sign in and Sign up fragments created

--- app/src/main/java/com/priyam/quantumassignment/MainActivity.kt
+++ app/src/main/java/com/priyam/quantumassignment/MainActivity.kt
@@ -2,10 +2,20 @@ package com.priyam.quantumassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager2: ViewPager2
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager2 = findViewById(R.id.viewPager2)
     }
 }
