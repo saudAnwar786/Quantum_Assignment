12645bf6337209fb50e22158fba7692f4af0c496
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-05 22:07:33 +0530

    Email and password login
    Initial Code

--- app/src/main/java/com/priyam/quantumassignment/SigninFragment.kt
+++ app/src/main/java/com/priyam/quantumassignment/SigninFragment.kt
@@ -1,15 +1,26 @@
 package com.priyam.quantumassignment
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SigninFragment : Fragment() {
-    lateinit var textView: TextView
+
+    lateinit var buttonSignin: Button
+    lateinit var editEmailSignin: EditText
+    lateinit var editPassSignin: EditText
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,9 +31,48 @@ class SigninFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.fragment_signin, container, false)
 
+        editEmailSignin = view.findViewById(R.id.editEmailSignIN)
+        editPassSignin = view.findViewById(R.id.editPassSignIn)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        buttonSignin = view.findViewById(R.id.signInBtn)
+        buttonSignin.setOnClickListener {
+            login()
+        }
+
 
 
         return view
+    }
+
+    private fun login(){
+        val email = editEmailSignin.text.toString()
+        val password = editPassSignin.text.toString()
+
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(context, "Some of the fields are blank.", Toast.LENGTH_SHORT)
+                .show()
+        }
+        (context as Activity?)?.let {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    it, OnCompleteListener<AuthResult?> { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                context,
+                                "Success",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        } else {
+                            Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
+        }
+
+
     }
 
 }
--- app/src/main/java/com/priyam/quantumassignment/SignupFragment.kt
+++ app/src/main/java/com/priyam/quantumassignment/SignupFragment.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -19,8 +21,9 @@ class SignupFragment : Fragment() {
     lateinit var editEmailSignUp: EditText
     lateinit var editNumberSignUp: EditText
     lateinit var editPassSignUp: EditText
+    lateinit var buttonSignup: Button
 
-    lateinit var firebaseAuth : FirebaseAuth
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,9 +36,14 @@ class SignupFragment : Fragment() {
         editEmailSignUp = view.findViewById(R.id.editEmailSignUp)
         editNumberSignUp = view.findViewById(R.id.editNumberSignUp)
         editPassSignUp = view.findViewById(R.id.editPassSignUp)
+        buttonSignup = view.findViewById(R.id.signUpBtn)
 
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        buttonSignup.setOnClickListener {
+            signupUser()
+        }
 
 
         return view
@@ -48,17 +56,32 @@ class SignupFragment : Fragment() {
         val email = editEmailSignUp.text.toString()
         val password = editPassSignUp.text.toString()
 
-        (context as Activity?)?.let {
-            firebaseAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(
-                    it,OnCompleteListener<AuthResult?> { task ->
-                        if (task.isSuccessful) {
-
-                        } else {
-
-                        }
-                    })
+        if (email.isBlank() || password.isBlank() || name.isBlank() || contact.isBlank()) {
+            Toast.makeText(context, "Some of the fields are blank or incorrect", Toast.LENGTH_SHORT)
+                .show()
+        } else if (password.length < 6) {
+            Toast.makeText(context, "Password too short", Toast.LENGTH_SHORT).show()
+        } else {
+            (context as Activity?)?.let {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(
+                        it, OnCompleteListener<AuthResult?> { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    context,
+                                    "Signed up successfully.",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                            } else {
+                                Toast.makeText(context, "Some error occurred.", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        })
+            }
         }
+
+
     }
 }
 
