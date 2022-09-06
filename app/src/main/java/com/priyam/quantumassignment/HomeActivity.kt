2e1aae45f3efb24517347fed8395cacc7d8f7eea
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-06 08:02:18 +0530

    Implemented Bottom Nav

--- app/src/main/java/com/priyam/quantumassignment/HomeActivity.kt
+++ app/src/main/java/com/priyam/quantumassignment/HomeActivity.kt
@@ -2,11 +2,24 @@ package com.priyam.quantumassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.priyam.quantumassignment.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bnvNews.setupWithNavController(
+            navController
+        )
 
     }
 }
